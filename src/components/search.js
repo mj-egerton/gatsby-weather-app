ec40c4baffebcea58856dca169ce8da6a822c911
mj-egerton <mjegerton.dev@gmail.com>
2021-05-05 22:09:32 +0100

    set up handlers for search component

--- src/components/search.js
+++ src/components/search.js
@@ -1,12 +1,20 @@
 import * as React from "react"
 
 const Search = () => {
+  const [searchValue, setSearchValue] = React.useState("");
+  const handleChange = (event) => {
+    setSearchValue(event.target.value)
+  }
+  const handleSubmit = (event) => {
+    event.preventDefault();
+  }
   return (
     <div>
         <label for="location-search"></label>
         <input type="search" id="location-search" name="location"
-            aria-label="Search weather conditions by location" />
-        <button>Search</button>
+            aria-label="Search weather conditions by location" placeholder="e.g. Manchester, London..."
+            onChange={handleChange} value={searchValue} />
+        <button onClick={handleSubmit}>Search</button>
     </div>
   )
 }
